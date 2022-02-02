4429da23962dbe7aa9f28b1592ffe422ebcc7ac5
Michael86868 <michael.kuhnel8@gmail.com>
2022-02-02 19:57:48 +0100

    Uploaded Functions library (Delay,CharToString...)

---
+++ lib/functions/functions.c
@@ -0,0 +1,31 @@
+#include "functions.h"
+
+volatile uint32_t Ticks;
+
+void SysTick_Handler(void) {
+  	Ticks++;
+}
+
+void Delay(uint32_t ms) {
+  	uint32_t start;
+  	start = Ticks;
+  	while ((Ticks - start) < ms);
+}
+
+char* charToString(unsigned char number){
+	static char string[50];
+	sprintf(string, "%c", number);
+	return string;
+}
+
+char* intToString(unsigned char number){
+	static char string[50];
+	sprintf(string, "%d", number);
+	return string;
+}
+
+char* floatToString(float number){
+	static char string[50];
+	sprintf(string, "%.2f", number);
+	return string;
+}
---
+++ lib/functions/functions.h
@@ -0,0 +1,12 @@
+#ifndef functions
+#define functions
+
+#include <stdio.h>
+
+void Delay(uint32_t ms);
+char* charToString(unsigned char number);
+char* intToString(unsigned char number);
+char* floatToString(float number);
+
+#endif
+
