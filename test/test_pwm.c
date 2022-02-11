3e29c1bf6678615251740e45e5a9fb2c86145651
Michael86868 <michael.kuhnel8@gmail.com>
2022-02-12 00:17:43 +0100

    Added test pwm program

---
+++ test/test_pwm.c
@@ -0,0 +1,30 @@
+#include "stm32f4xx.h"
+
+void GPIOD_Config(){
+    RCC->AHB1ENR |= (1UL << 2); // Povolení clocku na port C
+	GPIOC->MODER |= (2UL << 2*8) | (2UL << 2*9); // Nastavení alternativní funkce pro piny 8 a 9
+    GPIOC->OSPEEDR |= (3UL << 2*8) | (3UL << 2*9); // Nastavení frekvence 100MHz na 8 a 9 pin
+	GPIOC->PUPDR &= ~(3UL << 2*8) & ~(3UL << 2*9); // Vynulování registru (pro nastavení PULL UP)
+	GPIOC->PUPDR |= (1UL << 2*8) | (1UL << 2*9); // Nastavení PULL UP na 8 a 9 pin
+	GPIOC->AFR[1] |= (2UL << 4*0) | (2UL << 4*1); // Zapnutí alternativní funkce na piny 8 a 9
+}
+
+void TIM3_Config(){
+    RCC->APB1ENR |= (1UL << 1); //Zapnutí clocku na TIM3
+    TIM3->PSC = 15; // SysClock = 16/(15+1)=1MHz
+    TIM3->ARR = 10000; //Perioda = 10ms 
+    TIM3->EGR |= (1UL << 0); //Generate an update event  
+    TIM3->CCMR2 |= (6UL << 4) | (6UL << 12); //Zapnutí PWM  
+    TIM3->CCR3 = 4000;// Nastavení "duty cycle" na 40%
+    TIM3->CCR4 = 1000;// Nastavení "duty cycle" na 10%
+    TIM3->CCER |= (1UL << 0) | (1UL << 4) | (1UL << 8) | (1UL << 12);
+    TIM3->CR1 |= (1UL << 0); // Zapnutí citani citace	
+}
+
+
+int main(void)
+{ 
+    GPIOD_Config();
+    TIM3_Config();
+    for(;;);
+}
